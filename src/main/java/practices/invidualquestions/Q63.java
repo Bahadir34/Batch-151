97e7f6d56187002b2bac46f13cfb1d2a9526e996
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-15 18:58:51 +0300

    Q63 tamamlandi

--- src/main/java/practices/invidualquestions/Q63.java
+++ src/main/java/practices/invidualquestions/Q63.java
@@ -1,15 +1,27 @@
 package practices.invidualquestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q63
 {
 
     // String bir listede verilen tüm fiyatların toplamını bulunuz.
-    //
+    //{"$15.25" , "$21.85" , "$14.35" , "$32.75"}
 
     public static void main(String[] args)
     {
 
+        List<String> prices = List.of("$15.25" , "$21.85" , "$14.35" , "$32.75");
+        
+        double toplam = 0;
+        
+        for (String w : prices)
+        {
+            toplam += Double.valueOf(w.replace("$" , ""));
+        }
 
+        System.out.println("toplam = " + toplam);
 
     }
 
