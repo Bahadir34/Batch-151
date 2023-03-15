dd0724f4533974f502fb983c4f7355e5f61c5d9e
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-15 19:12:48 +0300

    Q64 tamamlandi

--- src/main/java/practices/invidualquestions/Q64.java
+++ src/main/java/practices/invidualquestions/Q64.java
@@ -1,10 +1,36 @@
 package practices.invidualquestions;
 
+import java.util.List;
+
 public class Q64
 {
     /*
 
     Bir string listede verilen en dusuk ve en buyuk fiyatın toplamını bulunuz.
-
      */
+
+    public static void main(String[] args)
+    {
+
+        List<String> prices = List.of("$15.25" , "$21.85" , "$14.35" , "$32.75");
+        double min = Double.valueOf(prices.get(0).replace("$" , ""));
+        double max = Double.valueOf(prices.get(0).replace("$" , ""));
+
+        for (String w : prices)
+        {
+            if(min < Double.valueOf(w.replace("$" , "")))
+            {
+                min = Double.valueOf(w.replace("$" , ""));
+            }
+
+            if(max > Double.valueOf(w.replace("$" , "")))
+            {
+                max = Double.valueOf(w.replace("$" , ""));
+            }
+        }
+
+        System.out.println("max = $" + max);
+        System.out.println("min = $" + min);
+
+    }
 }
