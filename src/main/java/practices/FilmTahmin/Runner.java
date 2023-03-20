2f275f13ecd5135212436290fda338fcda154e28
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-21 02:15:50 +0300

    Completed Rent a Car project.

---
+++ src/main/java/practices/FilmTahmin/Runner.java
@@ -0,0 +1,16 @@
+package practices.FilmTahmin;
+
+import java.util.Collections;
+
+import static practices.FilmTahmin.Methods.baslatMethod;
+
+public class Runner extends Depo {
+
+
+    public static void main(String[] args) {
+
+        baslatMethod();
+    }
+
+
+}
