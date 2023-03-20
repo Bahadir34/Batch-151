42206e5e2c8cb3ca9adb8911b8996bdd53fa92b7
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-21 02:18:08 +0300

    Completed Rent a Car Project

---
+++ src/main/java/practices/RentACar/Runner.java
@@ -0,0 +1,15 @@
+package practices.RentACar;
+
+
+import static practices.RentACar.Methods.calistir;
+
+
+
+public class Runner {
+
+    public static void main(String[] args) {
+        calistir();
+    }
+
+}
+
