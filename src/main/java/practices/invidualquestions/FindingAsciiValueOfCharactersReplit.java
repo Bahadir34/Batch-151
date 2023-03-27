3449ce99578053f2a98846d1811ddc9d795cdb69
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-27 04:11:48 +0300

    Alfabedeki karakterlerin kendisini ve ascii kodlarını gösteren kod yazıldı.

---
+++ src/main/java/practices/invidualquestions/FindingAsciiValueOfCharactersReplit.java
@@ -0,0 +1,14 @@
+package practices.invidualquestions;
+
+public class FindingAsciiValueOfCharactersReplit
+{
+    public static void main(String[] args)
+    {
+
+        for(int i = 97 ; i < 123 ; i++ )
+        {
+            System.out.println("Ascii value of " + (char)i + " = " + i);
+        }
+
+    }
+}
