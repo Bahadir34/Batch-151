d3f8a47e5ed75a5f24a12ccac28496ee83929746
Bahadır Ahmet Ulaş <bahadirahmetu@gmail.com>
2023-03-27 04:01:19 +0300

    Ekrana alfabeyi yazan program yazıldı.

---
+++ src/main/java/practices/invidualquestions/PrintingAlphabetReplit.java
@@ -0,0 +1,18 @@
+package practices.invidualquestions;
+
+public class PrintingAlphabetReplit
+{
+
+    /*
+    Print Alphabet on the console.
+
+    OUTPUT  : a b c .. .. .. .. y z
+     */
+    public static void main(String[] args)
+    {
+        for(int i = 97 ; i < 123 ; i++)
+        {
+            System.out.print((char)i + " ");
+        }
+    }
+}
